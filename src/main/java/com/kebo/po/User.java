b1adc522ca230b8382c01f86505b0ba5e677bbb6
Administrator <249183617@qq.com>
2020-11-18 09:55:51 +0800

    mybatis-plus

--- src/main/java/com/kebo/po/User.java
+++ src/main/java/com/kebo/po/User.java
@@ -36,5 +36,9 @@ public class User {
     @Version
     @TableField(fill = FieldFill.INSERT)
     private Integer version;
+
+    @TableLogic
+    @TableField(fill = FieldFill.INSERT)
+    private Integer deleted;
 }
 
--- src/test/java/MybatisPlusTests.java
+++ src/test/java/MybatisPlusTests.java
@@ -1,3 +1,5 @@
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.kebo.MybatisPlusApplication;
 import com.kebo.mapper.UserMapper;
@@ -6,10 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 /**
  * @description:
@@ -104,6 +103,120 @@ public class MybatisPlusTests {
         System.out.println(page.getTotal());
         System.out.println(page.hasNext());
         System.out.println(page.hasPrevious());
+    }
+
+    @Test
+    public void testSelectMapsPage() {
+        Page<User> page = new Page<>(1, 5);
+        IPage<Map<String, Object>> mapIPage = userMapper.selectMapsPage(page, null);
+        //注意：此行必须使用 mapIPage 获取记录列表，否则会有数据类型转换错误
+        mapIPage.getRecords().forEach(System.out::print);
+        System.out.println(page.getCurrent());
+        System.out.println(page.getPages());
+        System.out.println(page.getSize());
+        System.out.println(page.getTotal());
+        System.out.println(page.hasNext());
+        System.out.println(page.hasPrevious());
+    }
+
+    @Test
+    public void testDeleteById() {
+        int result = userMapper.deleteById(1L);
+        System.out.println(result);
+    }
+
+    @Test
+    public void deleteBatchIds() {
+        int result = userMapper.deleteBatchIds(Arrays.asList(2L, 3L));
+        System.out.println(result);
+    }
+
+    @Test
+    public void testDeleteByMap() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("name", "Helen");
+        map.put("age", 18);
+        int result = userMapper.deleteByMap(map);
+        System.out.println(result);
+    }
+
+    /* 测试逻辑删除*/
+    @Test
+    public void testLogicDelete() {
+        int result = userMapper.deleteById(5L);
+        System.out.println(result);
+    }
+
+    @Test
+    public void testDelete() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.isNotNull("name")
+                .ge("age", 12)
+                .isNotNull("email");
+        int result = userMapper.delete(queryWrapper);
+        System.out.println(result);
+    }
+
+    @Test
+    public void testSelectOne() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("name", "Sandy");
+        User user = userMapper.selectOne(queryWrapper);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testSelectCount() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.between("age", 20, 30);
+        Integer count = userMapper.selectCount(queryWrapper);
+        System.out.println(count);
+    }
+
+    @Test
+    public void testSelectList01() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", 2);
+        map.put("name", "Jack");
+        map.put("age", 20);
+        queryWrapper.allEq(map);
+        List<User> userList = userMapper.selectList(queryWrapper);
+        userList.forEach(System.out::print);
+    }
+
+    @Test
+    public void testSelectMaps() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.notLike("name", "e")
+                .likeRight("email", "t");
+        List<Map<String, Object>> maps = userMapper.selectMaps(queryWrapper);
+        maps.forEach(System.out::print);
+    }
+
+    @Test
+    public void testSelectObjs() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.in("id", 1, 2, 3);
+        //queryWrapper.inSql("id", "select id from user where id < 3");
+        List<Object> objects = userMapper.selectObjs(queryWrapper);//返回值是Object列表
+        objects.forEach(System.out::println);
+    }
+
+    @Test
+    public void testSelectListOrderBy() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.orderByDesc("id");
+        List<User> users = userMapper.selectList(queryWrapper);
+        users.forEach(System.out::print);
+    }
+
+    @Test
+    public void testSelectListColumn() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select("id", "name", "age");
+        List<User> users = userMapper.selectList(queryWrapper);
+        users.forEach(System.out::println);
     }
 }
 
